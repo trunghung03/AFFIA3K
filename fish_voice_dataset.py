ef51a1aac50e9430b419ccf7d440fd5a89cbdb48
Nguyen Trung Hung <61304518+trunghung03@users.noreply.github.com>
2026-09-16 13:42:32 +0700

    Refactor get_wav_name for better directory handling
    Refactor get_wav_name to simplify directory traversal and improve clarity.

--- fish_voice_dataset.py
+++ fish_voice_dataset.py
@@ -17,17 +17,19 @@ def load_audio(path, sr=None):
     return y
 
 def get_wav_name(split='strong'):
+    """
+    params: str
+        middle, none, strong, weak
+    """
     path = os.environ.get('FISH_AUDIO_PATH', '/kaggle/input/datasets/hungnguyentrung/fishfeedingintensity/Fish_feeding_sounds')
     audio = []
-    l1 = os.listdir(path)
-    for dir in l1:
-        full_dir_path = os.path.join(path, dir)
-        if not os.path.isdir(full_dir_path):
-            continue  # skip files like best.pt
-        l2 = os.listdir(full_dir_path)
-        for dir1 in l2:
-            wav_dir = os.path.join(path, dir, dir1, split, '*.wav')
-            audio.append(glob.glob(wav_dir))
+    split_path = os.path.join(path, split)
+    for dir1 in os.listdir(split_path):
+        full_path = os.path.join(split_path, dir1)
+        if not os.path.isdir(full_path):
+            continue  # skip stray files
+        wav_dir = os.path.join(full_path, '*.wav')
+        audio.append(glob.glob(wav_dir))
     return list(chain.from_iterable(audio))
 
 def data_generator(seed=20, test_sample_per_class=100):
